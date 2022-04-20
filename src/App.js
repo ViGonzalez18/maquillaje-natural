f4ea1479d9ffc24c10510d53b383a19780a4a28a
ViGonzalez18 <victoria.elicode@gmail.com>
2022-04-19 22:03:25 -0300

    muestra

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './componentes/Navbar';
+import Footer from './componentes/Footer';
+import Contenido from './componentes/Contenido';
 
 function App() {
   return (
-   <Navbar/>
+   <><Navbar />
+   <div>
+     <Contenido/>
+   </div>
+   <Footer /></>
   );
 }
 
---
+++ src/componentes/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='w-100 bg-dark text-light text-center p-5 my-5'>Todos los derechos están reservados © 2022</div>
+  )
+}
+
+export default Footer; 
